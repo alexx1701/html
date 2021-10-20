0d8885f639036e6f1f58161041db4c7d0888865d
alexx1701 <92756383+alexx1701@users.noreply.github.com>
2021-10-19 22:37:42 -0500

    Add files via upload

---
+++ html analytic.html/analytic.js
@@ -0,0 +1,13 @@
+//
+function buttonClicked()
+{
+var txt = document.getElementById('text').value;
+console.log(txt);
+}
+
+function imgButtonClicked()
+{
+
+var img = document.getElementById('image').value;
+console.log(img);
+}
